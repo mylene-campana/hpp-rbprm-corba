2285e5282582c337aef370109edf082f7b9ad687
Mylene Campana <mylene.campana@laas.fr>
2016-06-23 18:49:01 +0200

    Add Spiderman model (urdf, srdf, meshes, scripts)

---
+++ script/tests/spiderman_test_path.py
@@ -0,0 +1,208 @@
+#/usr/bin/env python
+# author: Mylene Campana (mcampana@laas.fr)
+# Script which goes with hpp-rbprm-corba package.
+# The script launches a skeleton-robot and a groundcrouch environment.
+# It defines init and final configs, and solve them with RBPRM.
+# Range Of Motions are spheres linked to the 4 end-effectors
+
+#blender/urdf_to_blender.py -p rbprmBuilder/ -i /local/mcampana/devel/hpp/src/animals_description/urdf/skeleton.urdf -o skeleton_blend.py
+
+
+from hpp.corbaserver import Client
+from hpp.corbaserver.rbprm.problem_solver import ProblemSolver
+from hpp.corbaserver.rbprm.rbprmbuilder import Builder
+from hpp.gepetto import Viewer, PathPlayer
+import math
+from viewer_library import *
+
+rootJointType = 'freeflyer'
+packageName = 'hpp-rbprm-corba'
+meshPackageName = 'hpp-rbprm-corba'
+urdfName = 'spiderman_trunk'
+urdfNameRoms = ['LFootSphere','RFootSphere']
+urdfSuffix = ""
+srdfSuffix = ""
+ecsSize = 4
+
+rbprmBuilder = Builder () # RBPRM
+rbprmBuilder.loadModel(urdfName, urdfNameRoms, rootJointType, meshPackageName, packageName, urdfSuffix, srdfSuffix)
+rbprmBuilder.setJointBounds ("base_joint_xyz", [-4, 5, -2, 2, -0.1, 2.7])
+rbprmBuilder.boundSO3([-0.2,0.2,-3.14,3.14,-0.3,0.3])
+rbprmBuilder.setFilter(urdfNameRoms)
+#rbprmBuilder.setNormalFilter('LFootSphere', [0,0,1], 0.5)
+#rbprmBuilder.setNormalFilter('RFootSphere', [0,0,1], 0.5)
+rbprmBuilder.client.basic.robot.setDimensionExtraConfigSpace(ecsSize)
+rbprmBuilder.client.basic.robot.setExtraConfigSpaceBounds([0,0,0,0,0,0,-3.14,3.14])
+
+ps = ProblemSolver (rbprmBuilder)
+r = Viewer (ps); gui = r.client.gui
+r(rbprmBuilder.getCurrentConfig ())
+
+pp = PathPlayer (rbprmBuilder.client.basic, r)
+r.loadObstacleModel ('hpp-rbprm-corba', "groundcrouch", "planning")
+ps.client.problem.selectPathValidation("RbprmPathValidation",0.05)
+addLight (r, [4,0,2,1,0,0,0], "li")
+
+# Configs : [x, y, z, q1, q2, q3, q4, dir.x, dir.y, dir.z, theta]
+q11 = rbprmBuilder.getCurrentConfig ()
+q11[(len(q11)-4):]=[0,0,1,0] # set normal for init / goal config
+q11[0:7] = [-3.0, 0, 0.9, 1, 0, 0, 0]; r(q11)
+
+rbprmBuilder.isConfigValid(q11)
+
+q22 = q11[::]
+q22[0:7] = [4, -1, 0.9, 1, 0, 0, 0]; r(q22)
+
+rbprmBuilder.isConfigValid(q22)
+
+
+#q1 = cl.rbprmBuilder.projectOnObstacle (q11, 0.001); q2 = cl.rbprmBuilder.projectOnObstacle (q22, 0.001)
+#q1 = rbprmBuilder.getCurrentConfig (); q2 = rbprmBuilder.getCurrentConfig ();
+#rbprmBuilder.isConfigValid(q1); rbprmBuilder.isConfigValid(q2); r(q1)
+
+
+ps.selectPathPlanner("PRMplanner")
+ps.client.problem.selectConFigurationShooter("RbprmShooter")
+ps.client.problem.setFrictionCoef(1.2); ps.client.problem.setMaxVelocityLim(5.2)
+ps.clearRoadmap();
+#ps.setInitialConfig (q11); ps.addGoalConfig (q22)
+
+# solve manually:
+waypoints = [[-1.557, 0.8453, 0.6329, 0.9925931769514134, 0.004237253738224199, 0.07407757915646249, 0.09619429825398208, 0.00940, -0.0187, 0.12716, 0.00983, 0.0018, -0.0546, 0.01716, -0.08254, -0.0419, -0.0108, -0.0483, 0.02347, -0.01099, 0.034956, 0.05726, -0.0578, 0.06128, -0.04249, -0.0058, 1.034000, -0.034469, 0.291741, 1.2386, -0.43000, -0.0, -0.0, 1.0, 0.0], [0.7720, 0.4444, 0.6215, 0.9974260830487915, 0.005979835741598267, -0.06517171780586004, 0.029293303273653835, 0.09103, -0.03612, 0.17486, 0.01930, 0.06663, 0.009278, 0.04751, -0.04233, 0.00427, -0.0726, 0.04350, -0.088241, -0.05880, -0.067950, 0.03791, -0.0391, 0.028666, 0.06231, -0.06085, 0.6511, -0.1964, 0.1932, -0.05359, -0.4356, 0.0, -0.0, 1.0, 0.0], [2.458539, 0.726329, 0.626443, 0.9946358490305908, -0.03129096926045146, 0.04766404686503832, -0.08630493440345526, 0.1388, -0.0846, 0.2457, -0.0162, 0.08825, 0.09928, -0.032699, -0.02511, -0.004535, -0.0950, 0.0301, 0.00442, 0.03061, 0.00062, -0.08391, 0.00844, 0.08017, 0.07035, 0.07338, -0.5080, -0.3888, -0.0634, 1.1029, -0.1095, 0.0, -0.0, 1.0, 0.0]]
+
+
+ps.setInitialConfig (q11); ps.addGoalConfig (waypoints [0]); ps.solve (); ps.resetGoalConfigs ()
+ps.setInitialConfig (waypoints [0]); ps.addGoalConfig (waypoints [1]); ps.solve (); ps.resetGoalConfigs ()
+ps.setInitialConfig (waypoints [1]); ps.addGoalConfig (waypoints [2]); ps.solve (); ps.resetGoalConfigs ()
+ps.setInitialConfig (waypoints [2]); ps.addGoalConfig (q22); ps.solve (); ps.resetGoalConfigs ()
+ps.setInitialConfig (q11); ps.addGoalConfig (q22)
+
+t = ps.solve ()
+solutionPathId = ps.numberPaths () - 1
+pp.displayPath(solutionPathId, [0.0, 0.0, 0.8, 1.0])
+
+print("Verify that all RB-waypoints are valid: ")
+pathWaypoints = ps.getWaypoints(solutionPathId)
+for i in range(1,len(pathWaypoints)-1):
+    if(not(rbprmBuilder.isConfigValid(pathWaypoints[i]))):
+        print('problem with waypoints number: ' + str(i))
+
+
+"""
+# Write data to log file
+pfr = ps.client.problem.getResultValues ()
+if isinstance(t, list):
+    timeSec = t[0]* 3600000 + t[1] * 60000 + t[2] * 1000 + t[3]
+f = open('log.txt', 'a')
+f.write("skeleton_test_path\n")
+f.write("path computation: " + str(timeSec) + "\n")
+f.write("parabola fail results: " + str(pfr) + "\n" + "\n")
+f.close()
+"""
+rob = rbprmBuilder.client.basic.robot
+r(q11)
+
+# Move RB-robot away in viewer
+qAway = q11 [::]; qAway[0] = -6
+rbprmBuilder.setCurrentConfig (qAway); r(qAway)
+
+
+## DEBUG tools ##
+"""
+cl.obstacle.getObstaclePosition('decor_base')
+rbprmBuilder.isConfigValid(q1)
+rbprmBuilder.setCurrentConfig(q1)
+res=rbprmBuilder.distancesToCollision()
+r( ps.configAtParam(0,5) )
+ps.optimizePath (0)
+ps.clearRoadmap ()
+ps.resetGoalConfigs ()
+from numpy import *
+argmin(rbprmBuilder.distancesToCollision()[0])
+rbprmBuilder.getJointNames ()
+rbprmBuilder.getConfigSize ()
+rbprmBuilder.client.rbprm.rbprm.isRbprmValid (q22)
+r.client.gui.getNodeList()
+rbprmBuilder.client.rbprm.rbprm.setRbShooter ()
+q = rbprmBuilder.client.rbprm.rbprm.rbShoot ()
+r(q)
+rbprmBuilder.client.rbprm.rbprm.isRbprmValid (q)
+
+rbprmBuilder.client.rbprm.rbprm.setRbShooter ()
+r(rbprmBuilder.client.rbprm.rbprm.rbShoot ())
+
+ps.client.problem.getResultValues ()
+"""
+
+## 3D viewer tools ##
+"""
+plotFrame (r, 'frame_group', [0,0,0], 0.6)
+
+gui.removeFromGroup("path0",r.sceneName)
+gui.getNodeList()
+ps.numberNodes()
+
+pathSamples = plotSampleSubPath (cl, r, pathId, 70, "path0", [0,0,1,1])
+plotCone (q1, cl, r, "cone_first", "friction_cone_SG2"); plotCone (q2, cl, r, "cone_second", "friction_cone_SG2")
+plotConeWaypoints (cl, pathId, r, "cone_wp_group", "friction_cone_WP2")
+
+# Plot cones and edges in viewer
+plotConesRoadmap (cl, r, 'cone_rm_group', "friction_cone2")
+plotEdgesRoadmap (cl, r, 'edgeGroup', 70, [0,1,0.2,1])
+
+gui = r.client.gui
+gui.setCaptureTransform ("frames.yaml ", ["skeleton_trunk_flexible"])
+q = q11
+r (q); cl.rbprmBuilder.setCurrentConfig(q)
+gui.refresh (); gui.captureTransform ()
+
+gui.setVisibility('skeleton_trunk_flexible/thorax_rhand_rom',"OFF")
+
+q = q_goal_test [0:7]
+q[0] = q[0] + 1; q[2] = q[2] + 1
+gui.addLight ("li", r.windowId, 0.0001, [0.9,0.9,0.9,1])
+gui.addToGroup ("li", r.sceneName)
+gui.applyConfiguration ("li", q)
+gui.refresh ()
+"""
+
+## Export path to BLENDER ##
+"""
+import numpy as np
+pathId = 0; dt = 0.05; gui.setCaptureTransform ("skeleton_trunk_path.yaml", ["skeleton_trunk_flexible"])
+PL = ps.pathLength(pathId)
+FrameRange = np.arange(0,PL,dt)
+numberFrame = len(FrameRange)
+
+# test frame capture
+q = q11; r (q); gui.refresh (); gui.captureTransform ()
+q = q22; r (q); gui.refresh (); gui.captureTransform ()
+
+# capture path
+for t in FrameRange:
+        q = ps.configAtParam (pathId, t)#update robot configuration
+        r (q); gui.refresh (); gui.captureTransform ()
+
+r (q22); gui.refresh (); gui.captureTransform ()
+"""
+
+""" # Manually add waypoints to roadmap:
+pbCl = rbprmBuilder.client.basic.problem
+pbCl.addConfigToRoadmap (waypoints[0])
+pbCl.addConfigToRoadmap (waypoints[1])
+pbCl.addConfigToRoadmap (waypoints[2])
+ps.directPath (q11, waypoints[0]); pathIds0 = ps.numberPaths () - 1
+ps.directPath (waypoints[0], waypoints[1]); pathId01 = ps.numberPaths () - 1
+ps.directPath (waypoints[1], waypoints[2]); pathId12 = ps.numberPaths () - 1
+ps.directPath (waypoints[2], q22); pathId2g = ps.numberPaths () - 1
+pbCl.addEdgeToRoadmap (q11, waypoints[0], pathIds0, True)
+pbCl.addEdgeToRoadmap (waypoints[0], waypoints[1], pathId01, True)
+pbCl.addEdgeToRoadmap (waypoints[1], waypoints[2], pathId12, True)
+pbCl.addEdgeToRoadmap (waypoints[2], q22, pathId2g, True)
+
+pbCl.saveRoadmap ('/local/mcampana/devel/hpp/data/skeleton_test_path.rdm')
+ps.readRoadmap ('/local/mcampana/devel/hpp/data/skeleton_test_path.rdm')
+"""
+
+
+
---
+++ script/tools/generateROMsSpiderman.py
@@ -0,0 +1,129 @@
+#/usr/bin/env python
+
+from hpp.corbaserver.rbprm.rbprmbuilder import Builder
+from hpp.corbaserver.rbprm.rbprmfullbody import FullBody
+from hpp.gepetto import Viewer
+from viewer_library import *
+
+import quaternion as quat
+
+packageName = "hpp-rbprm-corba"
+meshPackageName = "hpp-rbprm-corba"
+rootJointType = "freeflyer"
+##
+#  Information to retrieve urdf and srdf files.
+urdfName = "spiderman"
+urdfSuffix = ""
+srdfSuffix = ""
+
+fullBody = FullBody ()
+ 
+fullBody.loadFullBodyModel(urdfName, rootJointType, meshPackageName, packageName, urdfSuffix, srdfSuffix)
+
+from hpp.corbaserver.rbprm.problem_solver import ProblemSolver
+
+nbSamples = 10000
+
+ps = ProblemSolver( fullBody )
+
+r = Viewer (ps)
+q_0 = fullBody.getCurrentConfig (); r(q_0)
+
+# Verify that normals will be correct
+#cl = fullBody.client.basic
+#plotJointFrame (r, cl, q_0, "RFootSphere", 0.15); plotJointFrame (r, cl, q_0, "LFootSphere", 0.15)
+#plotJointFrame (r, cl, q_0, "RHandSphere", 0.15); plotJointFrame (r, cl, q_0, "LHandSphere", 0.15)
+
+x = 0.03 # contact surface width
+y = 0.08 # contact surface length
+
+#~ AFTER loading obstacles
+rLegId = 'rfoot'
+rLeg = 'RHip_J1'
+rfoot = 'RFootSphere'
+rLegOffset = [0,0,0]
+rLegNormal = [0,0,1]
+rLegx = x; rLegy = y
+fullBody.addLimb(rLegId,rLeg,rfoot,rLegOffset,rLegNormal, rLegx, rLegy, nbSamples, "EFORT", 0.01)
+
+lLegId = 'lfoot'
+lLeg = 'LHip_J1'
+lfoot = 'LFootSphere'
+lLegOffset = [0,0,0]
+lLegNormal = [0,0,1]
+lLegx = x; lLegy = y
+fullBody.addLimb(lLegId,lLeg,lfoot,lLegOffset,lLegNormal, lLegx, lLegy, nbSamples, "EFORT", 0.01)
+
+rarmId = 'rhand'
+rarm = 'RShoulder_J1'
+rHand = 'RHandSphere'
+rArmOffset = [0,0,0]
+rArmNormal = [1,0,0] # !! x, not z
+rArmx = x; rArmy = y
+fullBody.addLimb(rarmId,rarm,rHand,rArmOffset,rArmNormal, rArmx, rArmy, nbSamples, "EFORT", 0.01)
+
+larmId = 'lhand'
+larm = 'LShoulder_J1'
+lHand = 'LHandSphere'
+lArmOffset = [0,0,0]
+lArmNormal = [1,0,0] # !! x, not z
+lArmx = x; lArmy = y
+fullBody.addLimb(larmId,larm,lHand,lArmOffset,lArmNormal, lArmx, lArmy, nbSamples, "EFORT", 0.01)
+
+
+#make sure this is 0
+q_0 = fullBody.getCurrentConfig ()
+
+fullBody.createOctreeBoxes(r.client.gui, r.windowId, rarmId, q_0, [1,0.3,1,0.4])
+#fullBody.createOctreeBoxes(r.client.gui, r.windowId, rLegId, q_0, [1,1,0.3,0.4])
+#fullBody.createOctreeBoxes(r.client.gui, r.windowId, lLegId, q_0, [1,1,0.3,0.4])
+#fullBody.createOctreeBoxes(r.client.gui, r.windowId, larmId, q_0, [1,0.3,1,0.4])
+
+fullBody.draw (q_0, r) # display robot with octrees
+
+
+def printEffPosition(limbId, nbSamples):
+    limit = nbSamples-1;
+    f1=open('../../data/roms/spiderman/'+limbId+'.erom', 'w+')
+    for i in range(0,limit):
+        q = fullBody.getSamplePosition(limbId,i)
+        f1.write(str(q[0]) + "," + str(q[1]) + "," + str(q[2]) + "\n")
+    f1.close()
+
+
+printEffPosition(rarmId, nbSamples)
+printEffPosition(larmId, nbSamples)
+
+printEffPosition(rLegId, nbSamples)
+printEffPosition(lLegId, nbSamples)
+
+
+
+#to generate .obj from .erom with Matlab scripts
+#effectorRomToObj('../../data/roms/spiderman/rfoot.erom', '../../data/roms/spiderman/rfoot.obj')
+#effectorRomToObj('../../data/roms/spiderman/lfoot.erom', '../../data/roms/spiderman/lfoot.obj')
+#effectorRomToObj('../../data/roms/spiderman/lhand.erom', '../../data/roms/spiderman/lhand.obj')
+#effectorRomToObj('../../data/roms/spiderman/rhand.erom', '../../data/roms/spiderman/rhand.obj')
+
+# Then Decimate (object/Modifiers) the .obj in Blender and save it as .stl
+# Decimate decrease the too high number of facets.
+# Rotation corrections in Blender (in degrees): -90x for feet, 90y+90x for hands
+
+
+plotFrame (r, 'frame_group', [0,0,0], 0.6)
+
+frameGroupName = "frame"
+framePosition = [0,0,0]
+ampl = 0.6
+r.client.gui.createGroup (frameGroupName)
+x = framePosition [0]; y = framePosition [1]; z = framePosition [2];
+r.client.gui.addLine('frame1',[x,y,z], [x+ampl,y,z],[1,0,0,1])
+r.client.gui.addToGroup ('frame1', frameGroupName)
+r.client.gui.addLine('frame2',[x,y,z], [x,y+ampl,z],[0,1,0,1])
+r.client.gui.addToGroup ("frame2", frameGroupName)
+r.client.gui.addLine('frame3',[x,y,z], [x,y,z+ampl],[0,0,1,1])
+r.client.gui.addToGroup ('frame3', frameGroupName)
+r.client.gui.addSceneToWindow(frameGroupName,r.windowId)
+
+r.client.gui.setVisibility('spiderman/thorax_lhand_rom',"OFF")
+

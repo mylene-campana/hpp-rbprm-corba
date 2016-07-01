330ed12cccf1ed492a9f7456df85824b5fcf4804
Mylene Campana <mylene.campana@laas.fr>
2016-07-01 15:42:18 +0200

    Add Ant in Cave scenario

---
+++ script/tests/ant_cave_interp.py
@@ -0,0 +1,235 @@
+#/usr/bin/env python
+# author: Mylene Campana (mcampana@laas.fr)
+# Script which goes with hpp-rbprm-corba package.
+
+from hpp.corbaserver.rbprm.rbprmbuilder import Builder
+from hpp.corbaserver.rbprm.rbprmfullbody import FullBody
+from hpp.corbaserver.rbprm.problem_solver import ProblemSolver
+from hpp.gepetto import Viewer, PathPlayer
+import numpy as np
+from viewer_library import *
+
+import ant_test_path as tp
+
+
+packageName = 'hpp-rbprm-corba'
+meshPackageName = 'hpp-rbprm-corba'
+rootJointType = "freeflyer"
+##
+#  Information to retrieve urdf and srdf files.
+urdfName = "ant"
+urdfSuffix = ""
+srdfSuffix = ""
+ecsSize = tp.ecsSize
+V0list = tp.V0list
+Vimplist = tp.Vimplist
+
+fullBody = FullBody ()
+fullBody.loadFullBodyModel(urdfName, rootJointType, meshPackageName, packageName, urdfSuffix, srdfSuffix)
+fullBody.setJointBounds ("base_joint_xyz", [-6, 0, -2, 2, -0.3, 2.4])
+
+#psf = ProblemSolver(fullBody); rr = tp.Viewer (psf); gui = rr.client.gui
+r = tp.r; ps = tp.ps
+
+psf = tp.ProblemSolver( fullBody ); rr = tp.Viewer (psf); gui = rr.client.gui
+
+#~ AFTER loading obstacles
+nbSamples = 10000
+cType = "_6_DOF"
+x = 0.03 # contact surface width
+y = 0.03 # contact surface length
+# By default, all offset are set to [0,0,0] and all normals to [0,0,-1]
+
+lfLegId = 'lffoot'
+lfLeg = 'LFThigh_rx'
+lffoot = 'LFFootSphere'
+fullBody.addLimb(lfLegId,lfLeg,lffoot,[0,0,0],[0,0,-1], x, y, nbSamples, "EFORT_Normal", 0.01,cType)
+
+lmLegId = 'lmfoot'
+lmLeg = 'LMThigh_rx'
+lmfoot = 'LMFootSphere'
+fullBody.addLimb(lmLegId,lmLeg,lmfoot,[0,0,0],[0,0,-1], x, y, nbSamples, "EFORT_Normal", 0.01,cType)
+
+lbLegId = 'lbfoot'
+lbLeg = 'LBThigh_rx'
+lbfoot = 'LBFootSphere'
+fullBody.addLimb(lbLegId,lbLeg,lbfoot,[0,0,0],[0,0,-1], x, y, nbSamples, "EFORT_Normal", 0.01,cType)
+
+rfLegId = 'rffoot'
+rfLeg = 'RFThigh_rx'
+rffoot = 'RFFootSphere'
+fullBody.addLimb(rfLegId,rfLeg,rffoot,[0,0,0],[0,0,-1], x, y, nbSamples, "EFORT_Normal", 0.01,cType)
+
+
+rmLegId = 'rmfoot'
+rmLeg = 'RMThigh_rx'
+rmfoot = 'RMFootSphere'
+fullBody.addLimb(rmLegId,rmLeg,rmfoot,[0,0,0],[0,0,-1], x, y, nbSamples, "EFORT_Normal", 0.01,cType)
+
+rbLegId = 'rbfoot'
+rbLeg = 'RBThigh_rx'
+rbfoot = 'RBFootSphere'
+fullBody.addLimb(rbLegId,rbLeg,rbfoot,[0,0,0],[0,0,-1], x, y, nbSamples, "EFORT_Normal", 0.01,cType)
+print("Limbs added to fullbody")
+
+q_0 = fullBody.getCurrentConfig(); rr(q_0)
+
+
+confsize = len(tp.q11)-ecsSize
+fullConfSize = len(fullBody.getCurrentConfig()) # with or without ECS in fullbody
+q_init = fullBody.getCurrentConfig(); q_goal = q_init [::]
+
+# WARNING: q_init and q_goal may have changed in orientedPath
+entryPathId = tp.solutionPathId # tp.orientedpathId or tp.solutionPathId
+trunkPathwaypoints = ps.getWaypoints (entryPathId)
+q_init[0:confsize] = trunkPathwaypoints[0][0:confsize]
+q_goal[0:confsize] = trunkPathwaypoints[len(trunkPathwaypoints)-1][0:confsize]
+
+
+dir_init = [-V0list [0][0],-V0list [0][1],-V0list [0][2]] # first V0
+fullBody.setCurrentConfig (q_init)
+fullBody.isConfigValid(q_init)
+q_init_test = fullBody.generateContacts(q_init, dir_init, True); rr (q_init_test)
+fullBody.isConfigValid(q_init_test)
+
+dir_goal = (np.array(Vimplist [len(Vimplist)-1])).tolist() # last Vimp
+fullBody.setCurrentConfig (q_goal)
+q_goal_test = fullBody.generateContacts(q_goal, dir_goal, True); rr (q_goal_test)
+fullBody.isConfigValid(q_goal_test)
+
+fullBody.setStartState(q_init_test,[lfLegId,lmLegId,lbLegId,rfLegId,rmLegId,rbLegId])
+fullBody.setEndState(q_goal_test,[lfLegId,lmLegId,lbLegId,rfLegId,rmLegId,rbLegId])
+
+extending = [0.0, 0.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.1, 0.0, -0.5, 0.0, 0.0, 0.2, 0.0, 0.0, 1.1, 0.0, -0.5, 0.0, 0.0, 0.2, 0.0, 0.0, 1.1, 0.0, -0.5, 0.0, 0.0, 0.2, 0.0, 0.0, -1.1, 0.0, 0.5, 0.0, 0.0, 0.2, 0.0, 0.0, -1.1, 0.0, 0.5, 0.0, 0.0, 0.2, 0.0, 0.0, -1.1, 0.0, 0.5, 0.0, 0.0, 0.2, 0.0]
+flexion = [0.0, 0.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -0.6, 0.0, 0.6, 0.0, 0.0, 0.2, 0.0, 0.0, -0.6, 0.0, 0.6, 0.0, 0.0, 0.2, 0.0, 0.0, -0.6, 0.0, 0.6, 0.0, 0.0, 0.2, 0.0, 0.0, 0.6, 0.0, -0.6, 0.0, 0.0, 0.2, 0.0, 0.0, 0.6, 0.0, -0.6, 0.0, 0.0, 0.2, 0.0, 0.0, 0.6, 0.0, -0.6, 0.0, 0.0, 0.2, 0.0]
+fullBody.setPose (extending, "extending")
+fullBody.setPose (flexion, "flexion")
+
+print("Start ballistic-interpolation")
+fullBody.interpolateBallisticPath(entryPathId, 0.03)
+
+
+pp = PathPlayer (fullBody.client.basic, rr)
+pp.speed=0.6
+pp(psf.numberPaths ()-1)
+
+avNorm = fullBody.getnormalAverageVec() # verify that, with new contacts, averageConeNormals did not change too much
+
+fullBody.timeParametrizedPath(psf.numberPaths() -1 )
+pp(psf.numberPaths ()-1)
+
+
+"""
+fullBody.interpolateBetweenStates(state1, state2) # TODO tester
+pp(psf.numberPaths ()-1)
+
+fullBody.rotateAlongPath (psf.numberPaths ()-1) # TODO tester
+
+q11 [robot.rankInConfiguration ['RElbow_rx']] = -1.5
+
+# verify given offset position of contact-point
+q = q_init_test
+r(q)
+fullBody.setCurrentConfig (q)
+#posAtester = fullBody.client.basic.robot.computeGlobalPosition(fullBody.client.basic.robot.getJointPosition(rfoot),[0,0,0.2]); sphereName = "machin2"
+posAtester = fullBody.client.basic.robot.computeGlobalPosition(fullBody.client.basic.robot.getJointPosition(rHand),[0.1,0,0]); sphereName = "machin2"
+
+
+r.client.gui.addSphere (sphereName,0.03,[0.1,0.1,0.1,1]) # black
+configSphere = posAtester [::]
+configSphere.extend ([1,0,0,0])
+r.client.gui.applyConfiguration (sphereName,configSphere)
+r.client.gui.addToGroup (sphereName, r.sceneName)
+r.client.gui.refresh ()
+
+
+## Video recording
+import time
+pp.dt = 0.01
+pp.speed=0.5
+rr(q_init_test)
+rr.startCapture ("capture","png")
+rr(q_init_test); time.sleep(2)
+rr(q_init_test)
+pp(psf.numberPaths ()-1)
+rr(q_goal_test); time.sleep(2);
+rr.stopCapture ()
+
+## ffmpeg commands
+ffmpeg -r 30 -i capture_0_%d.png -r 25 -vcodec libx264 video.mp4
+x=0; for i in *png; do counter=$(printf %04d $x); ln "$i" new"$counter".png; x=$(($x+1)); done
+ffmpeg -r 30 -i new%04d.png -r 25 -vcodec libx264 video.mp4
+mencoder video.mp4 -channels 6 -ovc xvid -xvidencopts fixed_quant=4 -vf harddup -oac pcm -o video.avi
+ffmpeg -i untitled.mp4 -vcodec libx264 -crf 24 video.mp4
+
+
+## Export path to BLENDER
+pathId = 0; dt = 0.01; gui.setCaptureTransform ("skeleton_path.yaml", ["skeleton"])
+PL = ps.pathLength(pathId)
+FrameRange = np.arange(0,PL,dt)
+numberFrame = len(FrameRange)
+
+# test frame capture
+q = q_init_test; r (q); gui.refresh (); gui.captureTransform ()
+q = q_goal_test; r (q); gui.refresh (); gui.captureTransform ()
+
+# capture path
+for t in FrameRange:
+        q = ps.configAtParam (pathId, t)#update robot configuration
+        r (q); gui.refresh (); gui.captureTransform ()
+
+r (q_goal); robot.setCurrentConfig(q_goal); gui.refresh (); gui.captureTransform ()
+
+
+
+
+
+
+# flexion
+q [fullBody.rankInConfiguration ['ThoraxLFThigh_J2']] = -0.6; rr(q)
+q [fullBody.rankInConfiguration ['ThoraxLFShank_J2']] = 0.6; rr(q)
+q [fullBody.rankInConfiguration ['ThoraxLFFoot_J2']] = 0.2; rr(q)
+q [fullBody.rankInConfiguration ['ThoraxLMThigh_J2']] = -0.6; rr(q)
+q [fullBody.rankInConfiguration ['ThoraxLMShank_J2']] = 0.6; rr(q)
+q [fullBody.rankInConfiguration ['ThoraxLMFoot_J2']] = 0.2; rr(q)
+q [fullBody.rankInConfiguration ['ThoraxLBThigh_J2']] = -0.6; rr(q)
+q [fullBody.rankInConfiguration ['ThoraxLBShank_J2']] = 0.6; rr(q)
+q [fullBody.rankInConfiguration ['ThoraxLBFoot_J2']] = 0.2; rr(q)
+
+q [fullBody.rankInConfiguration ['ThoraxRFThigh_J2']] = 0.6; rr(q)
+q [fullBody.rankInConfiguration ['ThoraxRFShank_J2']] = -0.6; rr(q)
+q [fullBody.rankInConfiguration ['ThoraxRFFoot_J2']] = 0.2; rr(q)
+q [fullBody.rankInConfiguration ['ThoraxRMThigh_J2']] = 0.6; rr(q)
+q [fullBody.rankInConfiguration ['ThoraxRMShank_J2']] = -0.6; rr(q)
+q [fullBody.rankInConfiguration ['ThoraxRMFoot_J2']] = 0.2; rr(q)
+q [fullBody.rankInConfiguration ['ThoraxRBThigh_J2']] = 0.6; rr(q)
+q [fullBody.rankInConfiguration ['ThoraxRBShank_J2']] = -0.6; rr(q)
+q [fullBody.rankInConfiguration ['ThoraxRBFoot_J2']] = 0.2; rr(q)
+
+flexion = [0.0, 0.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -0.6, 0.0, 0.6, 0.0, 0.0, 0.2, 0.0, 0.0, -0.6, 0.0, 0.6, 0.0, 0.0, 0.2, 0.0, 0.0, -0.6, 0.0, 0.6, 0.0, 0.0, 0.2, 0.0, 0.0, 0.6, 0.0, -0.6, 0.0, 0.0, 0.2, 0.0, 0.0, 0.6, 0.0, -0.6, 0.0, 0.0, 0.2, 0.0, 0.0, 0.6, 0.0, -0.6, 0.0, 0.0, 0.2, 0.0]
+
+# extending
+q [fullBody.rankInConfiguration ['ThoraxLFThigh_J2']] = 1.1; rr(q)
+q [fullBody.rankInConfiguration ['ThoraxLFShank_J2']] = -0.5; rr(q)
+q [fullBody.rankInConfiguration ['ThoraxLFFoot_J2']] = 0.2; rr(q)
+q [fullBody.rankInConfiguration ['ThoraxLMThigh_J2']] = 1.1; rr(q)
+q [fullBody.rankInConfiguration ['ThoraxLMShank_J2']] = -0.5; rr(q)
+q [fullBody.rankInConfiguration ['ThoraxLMFoot_J2']] = 0.2; rr(q)
+q [fullBody.rankInConfiguration ['ThoraxLBThigh_J2']] = 1.1; rr(q)
+q [fullBody.rankInConfiguration ['ThoraxLBShank_J2']] = -0.5; rr(q)
+q [fullBody.rankInConfiguration ['ThoraxLBFoot_J2']] = 0.2; rr(q)
+
+q [fullBody.rankInConfiguration ['ThoraxRFThigh_J2']] = -1.1; rr(q)
+q [fullBody.rankInConfiguration ['ThoraxRFShank_J2']] = 0.5; rr(q)
+q [fullBody.rankInConfiguration ['ThoraxRFFoot_J2']] = 0.2; rr(q)
+q [fullBody.rankInConfiguration ['ThoraxRMThigh_J2']] = -1.1; rr(q)
+q [fullBody.rankInConfiguration ['ThoraxRMShank_J2']] = 0.5; rr(q)
+q [fullBody.rankInConfiguration ['ThoraxRMFoot_J2']] = 0.2; rr(q)
+q [fullBody.rankInConfiguration ['ThoraxRBThigh_J2']] = -1.1; rr(q)
+q [fullBody.rankInConfiguration ['ThoraxRBShank_J2']] = 0.5; rr(q)
+q [fullBody.rankInConfiguration ['ThoraxRBFoot_J2']] = 0.2; rr(q)
+
+extending = [0.0, 0.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.1, 0.0, -0.5, 0.0, 0.0, 0.2, 0.0, 0.0, 1.1, 0.0, -0.5, 0.0, 0.0, 0.2, 0.0, 0.0, 1.1, 0.0, -0.5, 0.0, 0.0, 0.2, 0.0, 0.0, -1.1, 0.0, 0.5, 0.0, 0.0, 0.2, 0.0, 0.0, -1.1, 0.0, 0.5, 0.0, 0.0, 0.2, 0.0, 0.0, -1.1, 0.0, 0.5, 0.0, 0.0, 0.2, 0.0]
+
+"""
+
---
+++ script/tests/ant_cave_path.py
@@ -0,0 +1,230 @@
+#/usr/bin/env python
+# author: Mylene Campana (mcampana@laas.fr)
+# Script which goes with hpp-rbprm-corba package.
+# The script launches a ant-robot and a groundcrouch environment.
+# It defines init and final configs, and solve them with RBPRM.
+# Range Of Motions are spheres linked to the 6 end-effectors
+
+
+from hpp.corbaserver import Client
+from hpp.corbaserver.rbprm.problem_solver import ProblemSolver
+from hpp.corbaserver.rbprm.rbprmbuilder import Builder
+from hpp.gepetto import Viewer, PathPlayer
+import math
+from viewer_library import *
+
+rootJointType = 'freeflyer'
+packageName = 'hpp-rbprm-corba'
+meshPackageName = 'hpp-rbprm-corba'
+urdfName = 'ant_trunk'
+urdfNameRoms = ['LFFootSphere','LMFootSphere','LBFootSphere','RFFootSphere','RMFootSphere','RBFootSphere']
+urdfSuffix = ""
+srdfSuffix = ""
+ecsSize = 4
+
+rbprmBuilder = Builder () # RBPRM
+rbprmBuilder.loadModel(urdfName, urdfNameRoms, rootJointType, meshPackageName, packageName, urdfSuffix, srdfSuffix)
+rbprmBuilder.setJointBounds ("base_joint_xyz", [-6, 0, -2, 2, 0.001, 2.4])
+rbprmBuilder.boundSO3([-3.14,3.14,-3.14,3.14,-3.14,3.14])
+rbprmBuilder.setFilter(urdfNameRoms)
+filterRange = 0.3
+rbprmBuilder.setNormalFilter('LFFootSphere', [0,0,1], filterRange)
+rbprmBuilder.setNormalFilter('LMFootSphere', [0,0,1], filterRange)
+rbprmBuilder.setNormalFilter('LBFootSphere', [0,0,1], filterRange)
+rbprmBuilder.setNormalFilter('RFFootSphere', [0,0,1], filterRange)
+rbprmBuilder.setNormalFilter('RMFootSphere', [0,0,1], filterRange)
+rbprmBuilder.setNormalFilter('RBFootSphere', [0,0,1], filterRange)
+rbprmBuilder.setContactSize (0.03,0.03)
+rbprmBuilder.client.basic.robot.setDimensionExtraConfigSpace(ecsSize)
+rbprmBuilder.client.basic.robot.setExtraConfigSpaceBounds([0,0,0,0,0,0,-3.14,3.14])
+
+ps = ProblemSolver (rbprmBuilder)
+ps.client.problem.selectPathValidation("RbprmPathValidation",0.05) # also configValidation
+rbprmBuilder.setNumberFilterMatch(4)
+r = Viewer (ps); gui = r.client.gui
+r(rbprmBuilder.getCurrentConfig ())
+
+pp = PathPlayer (rbprmBuilder.client.basic, r)
+r.loadObstacleModel ('hpp-rbprm-corba', "cave", "cave")
+addLight (r, [-3,0,8,1,0,0,0], "li");
+addLight (r, [-1,15,0,1,0,0,0], "li2");
+
+# Configs : [x, y, z, q1, q2, q3, q4, dir.x, dir.y, dir.z, theta]
+q11 = rbprmBuilder.getCurrentConfig ()
+q11[(len(q11)-4):]=[0,0,1,0] # set normal for init / goal config
+q11[0:7] = [0.8, -8.3, -4.55, 1, 0, 0, 0]; r(q11)
+
+rbprmBuilder.isConfigValid(q11)
+
+q22 = q11[::]
+q22[0:7] = [0.1, 14.2, -2.8, 1, 0, 0, 0]; r(q22)
+
+rbprmBuilder.isConfigValid(q22)
+
+ps.selectPathPlanner("BallisticPlanner")
+ps.client.problem.selectConFigurationShooter("RbprmShooter")
+rbprmBuilder.setFullOrientationMode(True) # RB-shooter follow obstacle-normal orientation
+rbprmBuilder.setFrictionCoef(1.2)
+rbprmBuilder.setMaxTakeoffVelocity(5)
+rbprmBuilder.setMaxLandingVelocity(10)
+ps.clearRoadmap();
+ps.setInitialConfig (q11); ps.addGoalConfig (q22)
+
+t = ps.solve ()
+solutionPathId = ps.numberPaths () - 1
+pp.displayPath(solutionPathId, [0.0, 0.0, 0.8, 1.0])
+
+rbprmBuilder.rotateAlongPath (solutionPathId)
+orientedpathId = ps.numberPaths () - 1
+#pp(orientedpathId)
+
+V0list = rbprmBuilder.getsubPathsV0Vimp("V0",solutionPathId)
+Vimplist = rbprmBuilder.getsubPathsV0Vimp("Vimp",solutionPathId)
+
+print("Verify that all RB-waypoints are valid: ")
+pathWaypoints = ps.getWaypoints(solutionPathId)
+for i in range(1,len(pathWaypoints)-1):
+    if(not(rbprmBuilder.isConfigValid(pathWaypoints[i])[0])):
+        print('problem with waypoints number: ' + str(i))
+
+
+plotConeWaypoints (rbprmBuilder, solutionPathId, r, "cone_wp_group", "friction_cone_WP2")
+plotCone (q11, rbprmBuilder, r, "cone_11", "friction_cone2"); plotCone (q22, rbprmBuilder, r, "cone_21", "friction_cone2")
+
+
+"""
+# Write data to log file
+pfr = rbprmBuilder.getResultValues ()
+if isinstance(t, list):
+    timeSec = t[0]* 3600000 + t[1] * 60000 + t[2] * 1000 + t[3]
+f = open('log.txt', 'a')
+f.write("ant_test_path\n")
+f.write("path computation: " + str(timeSec) + "\n")
+f.write("parabola fail results: " + str(pfr) + "\n" + "\n")
+f.close()
+"""
+rob = rbprmBuilder.client.basic.robot
+r(q11)
+
+# Move RB-robot away in viewer
+qAway = q11 [::]; qAway[1] = -11
+rbprmBuilder.setCurrentConfig (qAway); r(qAway)
+
+
+## DEBUG tools ##
+"""
+cl.obstacle.getObstaclePosition('decor_base')
+rbprmBuilder.isConfigValid(q1)
+rbprmBuilder.setCurrentConfig(q1)
+res=rbprmBuilder.distancesToCollision()
+r( ps.configAtParam(0,5) )
+ps.optimizePath (0)
+ps.clearRoadmap ()
+ps.resetGoalConfigs ()
+from numpy import *
+argmin(rbprmBuilder.distancesToCollision()[0])
+rbprmBuilder.getJointNames ()
+rbprmBuilder.getConfigSize ()
+rbprmBuilder.client.rbprm.rbprm.isRbprmValid (q22)
+r.client.gui.getNodeList()
+rbprmBuilder.client.rbprm.rbprm.setRbShooter ()
+q = rbprmBuilder.client.rbprm.rbprm.rbShoot ()
+r(q)
+rbprmBuilder.client.rbprm.rbprm.isRbprmValid (q)
+
+rbprmBuilder.client.rbprm.rbprm.setRbShooter ()
+r(rbprmBuilder.client.rbprm.rbprm.rbShoot ())
+
+ps.client.problem.getResultValues ()
+"""
+
+## 3D viewer tools ##
+"""
+plotFrame (r, 'frame_group', [0,0,0], 0.6)
+
+gui.removeFromGroup("path0",r.sceneName)
+gui.getNodeList()
+ps.numberNodes()
+
+pathSamples = plotSampleSubPath (cl, r, pathId, 70, "path0", [0,0,1,1])
+plotCone (q1, cl, r, "cone_first", "friction_cone_SG2"); plotCone (q2, cl, r, "cone_second", "friction_cone_SG2")
+plotConeWaypoints (rbprmBuilder, solutionPathId, r, "cone_wp_group", "friction_cone_WP2")
+
+# Plot cones and edges in viewer
+plotConesRoadmap (cl, r, 'cone_rm_group', "friction_cone2")
+plotEdgesRoadmap (cl, r, 'edgeGroup', 70, [0,1,0.2,1])
+
+gui = r.client.gui
+gui.setCaptureTransform ("frames.yaml ", ["skeleton_trunk_flexible"])
+q = q11
+r (q); cl.rbprmBuilder.setCurrentConfig(q)
+gui.refresh (); gui.captureTransform ()
+
+gui.setVisibility('skeleton_trunk_flexible/thorax_rhand_rom',"OFF")
+
+q = q_goal_test [0:7]
+q[0] = q[0] + 1; q[2] = q[2] + 1
+gui.addLight ("li", r.windowId, 0.0001, [0.9,0.9,0.9,1])
+gui.addToGroup ("li", r.sceneName)
+gui.applyConfiguration ("li", q)
+gui.refresh ()
+"""
+
+## Export path to BLENDER ##
+"""
+import numpy as np
+pathId = 0; dt = 0.05; gui.setCaptureTransform ("skeleton_trunk_path.yaml", ["skeleton_trunk_flexible"])
+PL = ps.pathLength(pathId)
+FrameRange = np.arange(0,PL,dt)
+numberFrame = len(FrameRange)
+
+# test frame capture
+q = q11; r (q); gui.refresh (); gui.captureTransform ()
+q = q22; r (q); gui.refresh (); gui.captureTransform ()
+
+# capture path
+for t in FrameRange:
+        q = ps.configAtParam (pathId, t)#update robot configuration
+        r (q); gui.refresh (); gui.captureTransform ()
+
+r (q22); gui.refresh (); gui.captureTransform ()
+"""
+
+""" # Manually add waypoints to roadmap:
+pbCl = rbprmBuilder.client.basic.problem
+pbCl.addConfigToRoadmap (waypoints[0])
+pbCl.addConfigToRoadmap (waypoints[1])
+pbCl.addConfigToRoadmap (waypoints[2])
+ps.directPath (q11, waypoints[0]); pathIds0 = ps.numberPaths () - 1
+ps.directPath (waypoints[0], waypoints[1]); pathId01 = ps.numberPaths () - 1
+ps.directPath (waypoints[1], waypoints[2]); pathId12 = ps.numberPaths () - 1
+ps.directPath (waypoints[2], q22); pathId2g = ps.numberPaths () - 1
+pbCl.addEdgeToRoadmap (q11, waypoints[0], pathIds0, True)
+pbCl.addEdgeToRoadmap (waypoints[0], waypoints[1], pathId01, True)
+pbCl.addEdgeToRoadmap (waypoints[1], waypoints[2], pathId12, True)
+pbCl.addEdgeToRoadmap (waypoints[2], q22, pathId2g, True)
+
+pbCl.saveRoadmap ('/local/mcampana/devel/hpp/data/skeleton_test_path.rdm')
+ps.readRoadmap ('/local/mcampana/devel/hpp/data/skeleton_test_path.rdm')
+
+
+# solve manually:
+waypoints = [[-3.699170692218081, -0.14040727877555925, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -1.0, -0.0, -0.0, 0.0]]
+ps.setInitialConfig (q11); ps.addGoalConfig (waypoints [0]); ps.solve (); ps.resetGoalConfigs ()
+
+for i in range(0,len(waypoints)-1):
+	ps.setInitialConfig (waypoints [i]); ps.addGoalConfig (waypoints [i+1]); ps.solve (); ps.resetGoalConfigs ()
+
+ps.setInitialConfig (waypoints [len(waypoints)-1]); ps.addGoalConfig (q22); ps.solve (); ps.resetGoalConfigs ()
+ps.setInitialConfig (q11); ps.addGoalConfig (q22)
+
+plotFrame (r, 'frame_group', [-2,0,0], 0.6)
+waypoints = [[-3.699170692218081, -0.14040727877555925, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -1.0, -0.0, -0.0, 0.0]]
+plotThetaPlane (q22, waypoints[0], r, "ThetaPlane")
+cl = rbprmBuilder.client.rbprm.rbprm
+plotCone (q22, cl, r, "c2", "friction_cone2")
+plotCone (waypoints[0], cl, r, "wp0", "friction_cone2")
+
+
+"""
+

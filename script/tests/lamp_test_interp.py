501ffc0ef339f0134c6a9490554da0686193668c
Mylene Campana <mylene.campana@laas.fr>
2016-08-18 17:39:47 +0200

    Add jumping lamp character

---
+++ script/tests/lamp_test_interp.py
@@ -0,0 +1,140 @@
+#/usr/bin/env python
+# author: Mylene Campana (mcampana@laas.fr)
+# Script which goes with hpp-rbprm-corba package.
+
+from hpp.corbaserver.rbprm.rbprmbuilder import Builder
+from hpp.corbaserver.rbprm.rbprmfullbody import FullBody
+from hpp.corbaserver.rbprm.problem_solver import ProblemSolver
+from hpp.gepetto import Viewer, PathPlayer
+import numpy as np
+from viewer_library import *
+
+import lamp_test_path as tp
+
+
+packageName = 'hpp-rbprm-corba'
+meshPackageName = 'hpp-rbprm-corba'
+rootJointType = "freeflyer"
+##
+#  Information to retrieve urdf and srdf files.
+urdfName = "lamp"
+urdfSuffix = ""
+srdfSuffix = ""
+ecsSize = tp.ecsSize
+V0list = tp.V0list
+Vimplist = tp.Vimplist
+base_joint_xyz_limits = tp.base_joint_xyz_limits
+
+fullBody = FullBody ()
+fullBody.loadFullBodyModel(urdfName, rootJointType, meshPackageName, packageName, urdfSuffix, srdfSuffix)
+fullBody.setJointBounds ("base_joint_xyz", base_joint_xyz_limits)
+fullBody.client.basic.robot.setDimensionExtraConfigSpace(ecsSize)
+fullBody.client.basic.robot.setExtraConfigSpaceBounds([0,0,0,0,0,0,-3.14,3.14])
+
+#psf = ProblemSolver(fullBody); rr = Viewer (psf)
+r = tp.r; ps = tp.ps
+psf = tp.ProblemSolver( fullBody ); rr = tp.Viewer (psf); gui = rr.client.gui
+q_0 = fullBody.getCurrentConfig(); rr(q_0)
+
+#~ AFTER loading obstacles
+nbSamples = 20000
+cType = "_6_DOF"
+x = 0.05 # contact surface width
+y = 0.05 # contact surface length
+offset = [0,0,0]
+normal = [0,0,1]
+
+LegId = 'foot'
+Leg = 'Thigh'
+foot = 'LampFootSphere'
+fullBody.addLimb(LegId, Leg, foot, offset, normal, x, y, nbSamples, "EFORT_Normal", 0.01, cType)
+print("Limbs added to fullbody")
+
+
+confsize = len(tp.q11)
+fullConfSize = len(fullBody.getCurrentConfig()) # with or without ECS in fullbody
+q_init = fullBody.getCurrentConfig(); q_goal = q_init [::]
+
+# WARNING: q_init and q_goal may have changed in orientedPath
+entryPathId = tp.orientedpathId # tp.orientedpathId or tp.solutionPathId
+trunkPathwaypoints = ps.getWaypoints (entryPathId)
+q_init[0:confsize-ecsSize] = trunkPathwaypoints[0][0:confsize-ecsSize]
+q_goal[0:confsize-ecsSize] = trunkPathwaypoints[len(trunkPathwaypoints)-1][0:confsize-ecsSize]
+if (ecsSize > 0):
+    q_init[fullConfSize-ecsSize:fullConfSize] = trunkPathwaypoints[0][confsize-ecsSize:confsize]
+    q_goal[fullConfSize-ecsSize:fullConfSize] = trunkPathwaypoints[len(trunkPathwaypoints)-1][confsize-ecsSize:confsize]
+
+
+dir_init = [-V0list [0][0],-V0list [0][1],-V0list [0][2]] # first V0
+fullBody.setCurrentConfig (q_init)
+fullBody.isConfigValid(q_init)
+q_init_test = fullBody.generateContacts(q_init, dir_init, True); rr (q_init_test)
+fullBody.isConfigValid(q_init_test)
+
+dir_goal = (np.array(Vimplist [len(Vimplist)-1])).tolist() # last Vimp
+fullBody.setCurrentConfig (q_goal)
+q_goal_test = fullBody.generateContacts(q_goal, dir_goal, True); rr (q_goal_test)
+fullBody.isConfigValid(q_goal_test)
+
+fullBody.setStartState(q_init_test,[LegId])
+fullBody.setEndState(q_goal_test,[LegId])
+
+extending = q_0 # TODO
+flexion = q_0 # TODO
+fullBody.setPose (extending, "extending")
+fullBody.setPose (flexion, "flexion")
+
+print("Start ballistic-interpolation")
+fullBody.interpolateBallisticPath(entryPathId, 0.01)
+
+
+pp = PathPlayer (fullBody.client.basic, rr)
+
+
+#fullBody.timeParametrizedPath(psf.numberPaths() -1)
+#pp.speed=1
+#pp(psf.numberPaths ()-1)
+
+
+"""
+## Export for Blender ##
+# First display in Viewer, then export
+# Don't change exported names, because harcoded in fullAnimationSkinning.py
+pathId = psf.numberPaths()-1 # path to export
+plotCone (q_init_test, psf, rr, "cone_start", "friction_cone2")
+plotCone (q_goal_test, psf, rr, "cone_goal", "friction_cone2")
+plotConeWaypoints (psf, pathId, r, "cone_wp_group", "friction_cone2")
+pathSamples = plotSampleSubPath (psf, rr, pathId, 70, "sampledPath", [1,0,0,1])
+
+gui.writeNodeFile('cone_wp_group','cones_path.dae')
+gui.writeNodeFile('cone_start','cone_start.dae')
+gui.writeNodeFile('cone_goal','cone_goal.dae')
+writePathSamples (pathSamples, 'path.txt')
+pathToYamlFile (psf, rr, "lampTest_frames.yaml ", "lamp", pathId, q_goal_test, 0.01)
+"""
+
+"""
+## Video recording
+import time
+pp.dt = 0.01
+pp.speed=0.1
+rr(q_init_test)
+rr.startCapture ("capture","png")
+rr(q_init_test); time.sleep(2)
+rr(q_init_test)
+pp(psf.numberPaths ()-1)
+rr(q_goal_test); time.sleep(2);
+rr.stopCapture ()
+
+## avconv commands
+avconv -r 50 -i capture_0_%d.png -r 15 -vcodec mpeg4 video.mp4
+x=0; for i in *png; do counter=$(printf %04d $x); ln "$i" new"$counter".png; x=$(($x+1)); done
+avconv -r 30 -i new%04d.png -r 25 -vcodec libx264 video.mp4
+mencoder video.mp4 -channels 6 -ovc xvid -xvidencopts fixed_quant=4 -vf harddup -oac pcm -o video.avi
+avconv -i video.mp4 -vcodec mpeg4 -crf 24 spiderman_jump_slowmo.mp4
+rm capture*.png
+rm video.mp4
+"""
+
+q = q_0 [::]
+q [fullBody.rankInConfiguration ['ShankJoint']] = 0.5;r(q)
---
+++ script/tests/lamp_test_path.py
@@ -0,0 +1,128 @@
+#/usr/bin/env python
+# author: Mylene Campana (mcampana@laas.fr)
+# Script which goes with hpp-rbprm-corba package.
+# The script launches a lamp-robot and a groundcrouch environment.
+# It defines init and final configs, and solve them with RBPRM.
+# Range Of Motions are spheres linked to the 6 end-effectors
+
+
+from hpp.corbaserver import Client
+from hpp.corbaserver.rbprm.problem_solver import ProblemSolver
+from hpp.corbaserver.rbprm.rbprmbuilder import Builder
+from hpp.gepetto import Viewer, PathPlayer
+import math
+from viewer_library import *
+
+rootJointType = 'freeflyer'
+packageName = 'hpp-rbprm-corba'
+meshPackageName = 'hpp-rbprm-corba'
+urdfName = 'lamp_trunk'
+urdfNameRoms = ['LampFootSphere']
+urdfSuffix = ""
+srdfSuffix = ""
+ecsSize = 4
+base_joint_xyz_limits = [-6, -1, -2, 2, 0.001, 2.4]
+
+rbprmBuilder = Builder () # RBPRM
+rbprmBuilder.loadModel(urdfName, urdfNameRoms, rootJointType, meshPackageName, packageName, urdfSuffix, srdfSuffix)
+rbprmBuilder.setJointBounds ("base_joint_xyz", base_joint_xyz_limits)
+#rbprmBuilder.boundSO3([-0.2,0.2,-3.14,3.14,-0.3,0.3])
+rbprmBuilder.boundSO3([-3.14,3.14,-3.14,3.14,-3.14,3.14])
+rbprmBuilder.setFilter(urdfNameRoms)
+filterRange = -1
+rbprmBuilder.setNormalFilter('LampFootSphere', [0,0,1], filterRange)
+rbprmBuilder.setContactSize (0.03,0.03)
+rbprmBuilder.client.basic.robot.setDimensionExtraConfigSpace(ecsSize)
+rbprmBuilder.client.basic.robot.setExtraConfigSpaceBounds([0,0,0,0,0,0,-3.14,3.14])
+
+ps = ProblemSolver (rbprmBuilder)
+ps.client.problem.selectPathValidation("RbprmPathValidation",0.05) # also configValidation
+rbprmBuilder.setNumberFilterMatch(1)
+r = Viewer (ps); gui = r.client.gui
+r(rbprmBuilder.getCurrentConfig ())
+
+pp = PathPlayer (rbprmBuilder.client.basic, r)
+r.loadObstacleModel ('hpp-rbprm-corba', "groundcrouch", "planning")
+addLight (r, [-3,0,8,1,0,0,0], "li");
+
+# Configs : [x, y, z, q1, q2, q3, q4, dir.x, dir.y, dir.z, theta]
+q11 = rbprmBuilder.getCurrentConfig ()
+q11[(len(q11)-4):]=[0,0,1,0] # set normal for init / goal config
+q11[0:7] = [-5.0, 0, 0.59, 1, 0, 0, 0]; r(q11)
+
+rbprmBuilder.isConfigValid(q11)
+
+#V = rbprmBuilder.computeConfigGIWC (q11,0.02,0.02)
+#plotGIWC (q11, V, r, 0, [0,1,0.1,1]) # attente reponse Steve
+
+q22 = q11[::]
+q22[0:7] = [-2.5, 0, 0.59, 1, 0, 0, 0]; r(q22)
+
+rbprmBuilder.isConfigValid(q22)
+
+#ps.selectPathPlanner("PRMplanner")
+ps.selectPathPlanner("BallisticPlanner")
+ps.client.problem.selectConFigurationShooter("RbprmShooter")
+rbprmBuilder.setFullOrientationMode(True) # RB-shooter follow obstacle-normal orientation
+rbprmBuilder.setFrictionCoef(1.2)
+rbprmBuilder.setMaxTakeoffVelocity(8)#(4.5)
+rbprmBuilder.setMaxLandingVelocity(8)
+ps.clearRoadmap();
+ps.setInitialConfig (q11); ps.addGoalConfig (q22)
+
+t = ps.solve ()
+solutionPathId = ps.numberPaths () - 1
+pp.displayPath(solutionPathId, [0.0, 0.0, 0.8, 1.0])
+
+rbprmBuilder.rotateAlongPath (solutionPathId)
+orientedpathId = ps.numberPaths () - 1
+#pp(orientedpathId)
+
+V0list = rbprmBuilder.getsubPathsV0Vimp("V0",solutionPathId)
+Vimplist = rbprmBuilder.getsubPathsV0Vimp("Vimp",solutionPathId)
+
+print("Verify that all RB-waypoints are valid: ")
+pathWaypoints = ps.getWaypoints(solutionPathId)
+for i in range(1,len(pathWaypoints)-1):
+    if(not(rbprmBuilder.isConfigValid(pathWaypoints[i])[0])):
+        print('problem with waypoints number: ' + str(i))
+
+
+plotConeWaypoints (ps, solutionPathId, r, "cone_wp_group", "friction_cone2")
+plotCone (q11, ps, r, "cone_11", "friction_cone2"); plotCone (q22, ps, r, "cone_21", "friction_cone2")
+
+
+
+# Write data to log file
+pfr = rbprmBuilder.getResultValues ()
+if isinstance(t, list):
+    timeSec = t[0]* 3600000 + t[1] * 60000 + t[2] * 1000 + t[3]
+f = open('log.txt', 'a')
+f.write("lamp_test_path\n")
+f.write("path computation: " + str(timeSec) + "\n")
+f.write("parabola fail results: " + str(pfr) + "\n" + "\n")
+f.close()
+
+rob = rbprmBuilder.client.basic.robot
+r(q11)
+
+# Move RB-robot away in viewer
+qAway = q11 [::]; qAway[0] = -6.5; qAway[1] = -2
+rbprmBuilder.setCurrentConfig (qAway); r(qAway)
+
+"""
+## Export for Blender ##
+# First display in Viewer, then export
+# Don't change exported names, because harcoded in fullAnimationSkinning.py
+pathId = ps.numberPaths()-1 # path to export
+plotCone (q_init_test, ps, r, "cone_start", "friction_cone2")
+plotCone (q_goal_test, ps, r, "cone_goal", "friction_cone2")
+plotConeWaypoints (ps, pathId, r, "cone_wp_group", "friction_cone2")
+pathSamples = plotSampleSubPath (ps.client.problem, r, pathId, 70, "sampledPath", [1,0,0,1])
+
+gui.writeNodeFile('cone_wp_group','cones_path.dae')
+gui.writeNodeFile('cone_11','cone_start2.dae')
+gui.writeNodeFile('cone_21','cone_goal2.dae')
+writePathSamples (pathSamples, 'lamp_path.txt')
+pathToYamlFile (psf, rr, "lampTrunkTest_frames.yaml ", "lamp_trunk", pathId, q_goal_test, 0.01)
+"""
